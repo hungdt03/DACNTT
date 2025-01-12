4feb2fb9176f8e57804438a45d052bb6578cf553
Do Van Hoang <dovanhoang0505@gmail.com>
2025-01-12 21:26:53 +0700

    test add group controller

---
+++ SocialNetwork.API/Controllers/GroupController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.API.Controllers
+{
+    [Route("api/groups")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public GroupController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+}
